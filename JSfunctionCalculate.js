08126ea5e6ae3248fe7e0dd0f5f00be10bfbedd9
Marina Postnikova <nerra@yandex.ru>
2019-11-02 22:40:44 +0300

    Add new JS file

---
+++ JSfunctionCalculate.js
@@ -0,0 +1,13 @@
+var calculate = function (firstNumber, secondNumber, operator) {
+ var result;
+ if (operator === '+') {
+  result = parseInt(firstNumber, 10) + parseInt(secondNumber, 10);
+  } else if (operator ===  '-') {
+     result = parseInt(firstNumber, 10) - parseInt(secondNumber, 10);
+   } else if (operator ===  '*') {
+      result = parseInt(firstNumber, 10) * parseInt(secondNumber, 10);
+    } else if (operator ===  '/') {
+   result = parseInt(firstNumber, 10) / parseInt(secondNumber, 10);
+     }
+ return result;
+} 
